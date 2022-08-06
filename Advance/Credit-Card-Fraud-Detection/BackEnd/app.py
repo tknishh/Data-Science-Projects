1e72bb010b1d3c788cc1b81cdbb9c2a9a4fb29fd
Tanish Khandelwal <76485783+tknishh@users.noreply.github.com>
2022-08-06 18:56:33 +0530

    Add files via upload

---
+++ Advance/Credit-Card-Fraud-Detection/BackEnd/app.py
@@ -0,0 +1,52 @@
+from fastapi import FastAPI, File, Query, UploadFile, HTTPException, Form
+from fastapi.responses import FileResponse, PlainTextResponse
+import uvicorn
+import joblib
+import numpy as np
+from pydantic import BaseModel
+
+
+
+app = FastAPI(
+    title="Credit Card Fraud Detection API",
+    description="""An API that utilises a Machine Learning model that detects if a credit card transaction is fraudulent or not based on the following features: hours, amount, transaction type etc.""",
+    version="1.0.0", debug=True)
+
+
+model = joblib.load('credit_fraud.pkl')
+
+@app.get("/", response_class=PlainTextResponse)
+async def running():
+  note = """
+Credit Card Fraud Detection API 🙌🏻
+Note: add "/docs" to the URL to get the Swagger UI Docs or "/redoc"
+  """
+  return note
+
+favicon_path = 'favicon.png'
+@app.get('/favicon.png', include_in_schema=False)
+async def favicon():
+    return FileResponse(favicon_path)
+																	
+class fraudDetection(BaseModel):
+    step:int
+    types:int
+    amount:float	
+    oldbalanceorig:float	
+    newbalanceorig:float	
+    oldbalancedest:float	
+    newbalancedest:float	
+    isflaggedfraud:float
+
+
+@app.post('/predict')
+def predict(data : fraudDetection):
+                                                                                                                                                                                                                                
+    features = np.array([[data.step, data.types, data.amount, data.oldbalanceorig, data.newbalanceorig, data.oldbalancedest, data.newbalancedest, data.isflaggedfraud]])
+    model = joblib.load('credit_fraud.pkl')
+
+    predictions = model.predict(features)
+    if predictions == 1:
+        return {"fraudulent"}
+    elif predictions == 0:
+        return {"not fraudulent"}
